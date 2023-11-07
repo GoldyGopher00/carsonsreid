ad2a5a29c18c7af50e2cc7e5bf5e0f14e340e466
GoldyGopher00 <52137193+GoldyGopher00@users.noreply.github.com>
2023-11-07 11:29:55 -0600

    Update chat.py with timeout

--- api/chat.py
+++ api/chat.py
@@ -1,47 +1,52 @@
-# Import necessary libraries
+import signal
+from contextlib import contextmanager
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
 import os
 
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Configure CORS with the environment variable for allowed origins, supporting comma-separated URLs
 allowed_origins = os.getenv('ALLOWED_ORIGINS', '*').split(',')
 CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
-# Load the OpenAI API key from an environment variable
 openai.api_key = os.getenv('OPENAI_API_KEY')
-
-# Get the system message from an environment variable
 SYSTEM_MESSAGE = os.getenv('SYSTEM_MESSAGE', 'You are a helpful assistant.')
 
-# Define the route for the chat API
+# Create a context manager to timeout function execution
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutError("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
 @app.route('/api/chat', methods=['POST'])
 def chat():
-    # Parse the incoming JSON data
     data = request.json
     messages = data.get('messages', [])
 
-    # Add the system message at the start of the conversation
     system_message = {
         "role": "system",
         "content": SYSTEM_MESSAGE
     }
     messages.insert(0, system_message)
 
-    # Call the OpenAI API with the conversation messages and return the response
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-4",  # Use GPT-4 model
-            messages=messages
-        )
+        with time_limit(60):
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=messages
+            )
         return jsonify(response)
+    except TimeoutError:
+        return jsonify({'error': 'Sorry, it looks like OpenAI took too long. Please try again. If the issue persists, please refresh the page.'}), 504
     except openai.error.OpenAIError as e:
-        # If there is an API error, return the error message with a 500 status code
         return jsonify({'error': str(e)}), 500
 
-# Run the Flask app if this file is executed as the main program
 if __name__ == '__main__':
     app.run()
